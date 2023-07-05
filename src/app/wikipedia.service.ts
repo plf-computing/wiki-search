01cfc508fc4bbb328ab31921cb502dca9b8ecae2
plf-computing <plfcomputing@gmail.com>
2023-07-05 14:57:24 +0300

    last commit

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { query } from '@angular/animations';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title:string;
+      snippet:string;
+      pageid: number;
+    }[];
+  }
+}
+
+// const observable = new Observable<number>((observer) =>{
+//   observer.next(1);
+// });
+// observable.subscribe(value => {
+//   console.log(value);
+// });
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
+  
 
   constructor(private http: HttpClient ) { }
 
 
   search(term:string){
-    return this.http.get('https://en.wikipedia.org/w/api.php'),{
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php',{
       params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf: '1',
+        action:'query',
+        format:'json',
+        list:'search',
+        utf8: '1',
         srsearch: term,
         origin: '*'
       }
-    }
+
+    }).pipe(
+      map(WikipediaResponse =>WikipediaResponse.query?.search)
+    );
+    
   }
 }
